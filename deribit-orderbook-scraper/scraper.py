af8a23dfc59f8ade2301c86b13c1bbaf1ccf82d5
Faizaan Pervaiz <fp361@cam.ac.uk>
2020-03-29 18:35:41 +0100

    Major update

--- deribit-orderbook-scraper/scraper.py
+++ deribit-orderbook-scraper/scraper.py
@@ -1,7 +1,8 @@
 import csv
 import json
+import logging
+import logging.handlers
 import os.path
-#import pandas as pd
 import openapi_client as dbitApi
 
 from time import sleep
@@ -16,38 +17,36 @@ kind = 'option'
 expired = 'false'
 
 # Output destinations
-order_data_folder = './deribit-orderbook-scraper/scraped_data'
+order_data_folder = '~/cloud-mounts/deribit_scraped_data'
+output_log_path = 'deribit_scraper.log'
+
+#### ---------- ####
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+file_handler = logging.handlers.RotatingFileHandler(output_log_path, maxBytes=8000000, backupCount=5)
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 api = dbitApi.MarketDataApi()
 
-# Get instrument selection
-try:
-    instrument_result = api.public_get_instruments_get(currency, kind=kind, expired=expired)['result']
-except dbitApi.exceptions.ApiException as e:
-    print("Exception when calling MarketDataApi->public_get_instruments_get: {}}\n".format(e))
-    exit()
+def get_instruments(currency, kind, expired):
+    try:
+        instrument_result = api.public_get_instruments_get(currency, kind=kind, expired=expired)['result']
+        return [instrument['instrument_name'] for instrument in instrument_result]
+    except dbitApi.exceptions.ApiException as e:
+        logger.exception('Exception when calling MarketDataApi->public_get_instruments_get!')
+        exit()
 
-instruments = [instrument['instrument_name'] for instrument in instrument_result]
-
-def scrape(instrument):
+def scrape_orderbook(instrument):
     try:
         orderbook_result = api.public_get_order_book_get(instrument)['result']
     except dbitApi.exceptions.ApiException as e:
-        print("Exception when calling MarketDataApi->public_get_instruments_get: {}}\n".format(e))
-        return
-    
-    ''' TO DO: dump JSON to preserve non flat fields (e.g. ask/bid prices, greeks, stats) without manipulation
-
-    with open(order_data_folder + '/{}.json'.format(instrument), 'r+') as f:
-        content = json.load(f)
-        if not content:
-            # Is empty so initialise
-            json.dump([], f)
-        content = json.load(f)
-        content.append(orderbook_result)
-        json.dump(content, f)
-
-    '''
+        logger.exception('Exception when calling MarketDataApi->public_get_instruments_get!')
+        return False
 
     # Flatten all fields and append to CSV
 
@@ -56,9 +55,10 @@ def scrape(instrument):
         orderbook_result['24h_high'] = orderbook_result['stats']['high']
         orderbook_result['24h_low'] = orderbook_result['stats']['low']
         orderbook_result['24h_vol'] = orderbook_result['stats']['volume']
+        orderbook_result['24h_change'] = orderbook_result['stats']['price_change']
         del orderbook_result['stats']
     except KeyError as e:
-        print('{}: {}'.format(instrument, e))
+        logger.exception('{}: Error parsing stats'.format(instrument))
 
     # Greeks
     try:
@@ -66,7 +66,7 @@ def scrape(instrument):
             orderbook_result[k] = v
         del orderbook_result['greeks']
     except KeyError as e:
-        print('{}: {}'.format(instrument, e))
+        logger.exception('{}: Error parsing greeks'.format(instrument))
 
     # Asks and bids
     orderbook_result['asks'] = str(orderbook_result['asks'])
@@ -77,32 +77,46 @@ def scrape(instrument):
 
     try:
         with open(filename, 'a+', newline='') as f:
-            headers = ['underlying_price', 'underlying_index', 'timestamp', 'state', 'settlement_price', 'open_interest', 'min_price', 'max_price', 'mark_price', 'mark_iv', 'last_price', 'interest_rate', 'instrument_name', 'index_price', 'change_id', 'bids', 'bid_iv', 'best_bid_price', 'best_bid_amount', 'best_ask_price', 'best_ask_amount', 'asks', 'ask_iv', '24h_high', '24h_low', '24h_vol', 'theta', 'delta', 'rho', 'gamma', 'vega']
+            headers = ['timestamp', 'instrument_name', 'state', 'index_price', 'underlying_index', 'underlying_price', 'settlement_price', 'estimated_delivery_price', 'delivery_price', 'open_interest', 'bids', 'bid_iv', 'best_bid_price', 'best_bid_amount', 'asks', 'ask_iv', 'best_ask_price', 'best_ask_amount', 'min_price', 'max_price', 'mark_price', 'mark_iv', 'last_price', 'interest_rate', 'change_id', '24h_high', '24h_low', '24h_vol', '24h_change', 'theta', 'delta', 'rho', 'gamma', 'vega']
             writer = csv.DictWriter(f, fieldnames=headers)
 
             if not csv_exists:
                 writer.writeheader()
 
             writer.writerow(orderbook_result)
-            print('Successfully updated {}'.format(filename))
+            logger.info('Successfully updated {}'.format(filename))
 
     except PermissionError:
-        # File unwritable for some temporary reason (e.g. been opened by another process) so skip
-        print('Skipping {}: file unavailable')
+        logger.exception('Skipping {}: file unavailable')
+
+    return orderbook_result['state'] == 'closed'
 
 if __name__ == '__main__':
-    try:
-        while True:
-            for instrument in instruments:
-                try:
-                    scrape(instrument)
-                except Exception as e:
-                    print('Unhandled exception for {}: {}'.format(instrument, e))
-                    print('Skipping {}...'.format(instrument))
-                    continue
-            print('Updates complete. Waiting {} seconds...'.format(resolution))
+    logger.info('===================================================')
+    logger.info('BitFEAR Deribit Orderbook Scraper')
+    logger.info('Params: resolution={} currency={} kind={} expired={}'.format(resolution, currency, kind, expired))
+
+    instruments = get_instruments(currency, kind, expired)
+    logger.info('Got {} instruments'.format(len(instruments)))
+
+    while True:
+        for instrument in instruments:
+            try:
+                expired = scrape_orderbook(instrument)
+                if expired:
+                    instruments.remove(instrument)
+            except KeyboardInterrupt:
+                logger.info("Exiting...")
+                exit()
+            except Exception as e:
+                logger.exception('Unhandled exception for {}!'.format(instrument))
+                logger.info('Skipping {}...'.format(instrument))
+                continue 
+
+        logger.info('Updates complete. Waiting {} seconds...'.format(resolution))
+
+        try:
             sleep(resolution)
-    except KeyboardInterrupt:
-        # Exit gracefully
-        print("Exiting...")
-        exit()
+        except KeyboardInterrupt:
+            logger.info("Exiting...")
+            exit()
