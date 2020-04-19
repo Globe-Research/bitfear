6b97b53f0d07c0adade02b391288d6969fba74d5
Faizaan Pervaiz <fp361@cam.ac.uk>
2020-04-19 21:04:45 +0100

    Do not write index

--- vix-implementation/live_indices.py
+++ vix-implementation/live_indices.py
@@ -8,7 +8,7 @@ def write_indices():
     timestamp = datetime.now().replace(second=0, microsecond=0)
     vxbt, gvxbt, avxbt = vc.get_indices()
     df = pd.DataFrame({'timestamp': timestamp, 'vxbt': vxbt, 'gvxbt': gvxbt, 'avxbt': avxbt}, index=[0])
-    df.to_csv('live_indices.csv', mode='a', header=False)
+    df.to_csv('live_indices.csv', mode='a', header=False, index=False)
 
 scheduler = BlockingScheduler()
 scheduler.add_job(write_indices, 'cron', minute='*')
