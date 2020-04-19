6051f719c89cbafd2b2e31c68d21b4b3c77e2e38
Faizaan Pervaiz <fp361@cam.ac.uk>
2020-04-19 20:55:33 +0100

    Live index calculations

---
+++ vix-implementation/live_indices.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+from datetime import datetime
+from vxbt_calc import vxbt_calc as vc
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def write_indices():
+    timestamp = datetime.now().replace(second=0, microsecond=0)
+    vxbt, gvxbt, avxbt = vc.get_indices()
+    df = pd.DataFrame({'timestamp': timestamp, 'vxbt': vxbt, 'gvxbt': gvxbt, 'avxbt': avxbt})
+    df.to_csv('live_indices.csv', mode='a', header=False)
+
+scheduler = BlockingScheduler()
+scheduler.add_job(write_indices, 'cron', minute='*')
+scheduler.start()
