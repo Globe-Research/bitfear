936ae44f3668e324b448d94adbd3b095539d27b7
Faizaan Pervaiz <fp361@cam.ac.uk>
2020-04-19 21:01:08 +0100

    Index bug fix

--- vix-implementation/live_indices.py
+++ vix-implementation/live_indices.py
@@ -7,7 +7,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 def write_indices():
     timestamp = datetime.now().replace(second=0, microsecond=0)
     vxbt, gvxbt, avxbt = vc.get_indices()
-    df = pd.DataFrame({'timestamp': timestamp, 'vxbt': vxbt, 'gvxbt': gvxbt, 'avxbt': avxbt})
+    df = pd.DataFrame({'timestamp': timestamp, 'vxbt': vxbt, 'gvxbt': gvxbt, 'avxbt': avxbt}, index=[0])
     df.to_csv('live_indices.csv', mode='a', header=False)
 
 scheduler = BlockingScheduler()
